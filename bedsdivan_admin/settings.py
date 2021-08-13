d615b9d84bc010f5afd64caa0b83f7bc522fb5ee
ProgrammingInBlood <eklavyasingh12065@gmail.com>
2021-08-13 14:15:17 +0530

    1st commit by Eklavya

--- bedsdivan_admin/settings.py
+++ bedsdivan_admin/settings.py
@@ -171,10 +171,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 102400
